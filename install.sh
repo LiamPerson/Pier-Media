8b6db4589e1598076a6d75bfc88bc1cfd7a3a73c
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-10-02 14:47:15 +1000

    added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Installs all necessary packages for Node Pier
+
+EXECUTION_LOCATION=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo -e "Attempting to install all dependencies for Node Pier"
+npm install
+cd "$EXECUTION_LOCATION/client"
+npm install
+echo -e "\n\n\nAll processes complete!\nExiting...\n"
