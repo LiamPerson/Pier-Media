67b9de99898cc8b70f390fe379f8c05947de442a
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-10-02 16:43:53 +1000

    added tooltip example to helloworld

--- client/src/components/pages/HelloWorld/HelloWorld.component.jsx
+++ client/src/components/pages/HelloWorld/HelloWorld.component.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import useTooltipPositioner from "../../../hooks/useTooltipPositioner.hook";
 import { Button } from "../../templates/Button/Button.style";
+import Tooltip from "../../templates/Tooltip/Tooltip.component";
 import { HelloWorldContainer } from "./HelloWorld.style";
 
-const HelloWorld = () => (
-    <HelloWorldContainer>
-        <p>Hello, world! <Button color="">Sample Button</Button></p>
-    </HelloWorldContainer>
-)
+const HelloWorld = () => {
+
+    const [buttonRef, tooltipPositioner] = useTooltipPositioner();
+    
+    return (<HelloWorldContainer>
+        <div>
+            Hello, world! 
+            <Button ref={buttonRef} color="">Sample Button</Button>
+            <Tooltip position={tooltipPositioner}>Deez nuts</Tooltip>
+        </div>
+    </HelloWorldContainer>)
+}
 
 export default HelloWorld;
