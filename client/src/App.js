c0ff4c2e9820942edb5bf5821f7feeeddc788b9e
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-10-02 16:19:58 +1000

    added routes and redux to main app with loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import HelloWorld from './components/pages/HelloWorld/HelloWorld.component';
+// Libraries
+import React, { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// Local Components
+import { Spinner } from './components/templates/Loader/Loader.style';
+
+// Pages
+const HelloWorld = lazy(() => import('./components/pages/HelloWorld/HelloWorld.component'));
 
 function App() {
-  return (
-      <HelloWorld />
-  );
+  return (<Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/' component={HelloWorld} />
+        </Suspense>
+      </Switch>
+    </BrowserRouter>
+  </Provider>);
 }
 
 export default App;
