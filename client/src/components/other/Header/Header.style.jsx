63097edf29f0b5d639875e85d4f44edf8c2633ad
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2022-10-03 13:39:24 +1000

    made PierLogo link to home

--- client/src/components/other/Header/Header.style.jsx
+++ client/src/components/other/Header/Header.style.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import style from '../../../settings/style.json';
+import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.div`
   background-color: ${style.color["dark-3"]};
@@ -20,9 +21,11 @@ export const BurgerMenuWrapper = styled.div`
     justify-content: center;
 `;
 
-export const PierLogo = styled.img.attrs({
-    src: "/logo192.png"
+export const PierLogo = styled(Link).attrs({
+  to: "/"
 })`
+  background: url("/logo192.png") no-repeat;
+  background-size: contain;
   width: 36px;
   height: 36px;
   margin: 7px;
