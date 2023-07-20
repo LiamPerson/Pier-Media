7d46181b92b7cf910317602c6980beb4b54c0ea1
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-21 00:51:51 +1000

    allowed SX to be passed to sticky

--- components/DashboardPage/Sticky.tsx
+++ components/DashboardPage/Sticky.tsx
@@ -1,13 +1,15 @@
-import { Box } from '@mui/material'
+import { Box, BoxProps } from '@mui/material'
 
 interface StickyProps {
 	children: React.ReactNode
+	sx?: BoxProps['sx']
 }
-const Sticky = ({ children }: StickyProps) => {
+const Sticky = ({ children, sx }: StickyProps) => {
 	return (
 		<Box
 			position={'sticky'}
 			top={0}
+			sx={sx}
 		>
 			{children}
 		</Box>
