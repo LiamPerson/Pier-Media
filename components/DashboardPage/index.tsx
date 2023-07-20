1760944ce4c31229d4513e4a8ee5c6eee0eef4ea
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-21 01:56:13 +1000

    made navbar follow sticky sidebar

--- components/DashboardPage/index.tsx
+++ components/DashboardPage/index.tsx
@@ -3,6 +3,7 @@ import BasePage from '../BasePage'
 import StickyPage from './StickyPage'
 import Sticky from './Sticky'
 import Banner from '../Banner'
+import { useElementSize } from 'usehooks-ts'
 
 const SIDEBAR_WIDTH = '150px'
 
@@ -10,12 +11,16 @@ interface DashboardPageProps {
 	children: React.ReactNode
 }
 const DashboardPage = ({ children }: DashboardPageProps) => {
+	/** @todo - I should refactor this at some point. I couldn't figure out how to get the header to be the correct size with pure css. */
+	const [pageRef, { width: windowWidth }] = useElementSize()
 	return (
-		<BasePage>
+		<BasePage ref={pageRef}>
 			<Banner />
 			<StickyPage>
 				<Sticky sx={{ height: '100vh', zIndex: 1 }}>
-					<Paper sx={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0 }}>9999999999999999999999999999</Paper>
+					<Paper sx={{ position: 'absolute', top: 0, left: 0, width: windowWidth, background: 'yellow' }}>
+						9999999999999999999999999999
+					</Paper>
 					<Paper sx={{ position: 'absolute', top: 0, left: 0, bottom: 0, width: SIDEBAR_WIDTH }}>side</Paper>
 				</Sticky>
 				<Box marginLeft={SIDEBAR_WIDTH}>{children}</Box>
