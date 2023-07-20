fd76ee6e619593075603f605e21022487ae645c2
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-21 01:55:08 +1000

    forwarded ref on basepage

--- components/BasePage/index.tsx
+++ components/BasePage/index.tsx
@@ -1,15 +1,19 @@
 import Head from '@/components/BasePage/Head'
+import { Box } from '@mui/material'
+import { forwardRef } from 'react'
 
 interface GenericPageProps {
 	children: React.ReactNode
 }
-const BasePage = ({ children }: GenericPageProps) => {
+const BasePage = forwardRef(({ children }: GenericPageProps, ref) => {
 	return (
-		<>
+		<Box ref={ref}>
 			<Head />
 			{children}
-		</>
+		</Box>
 	)
-}
+})
+
+BasePage.displayName = 'BasePage'
 
 export default BasePage
