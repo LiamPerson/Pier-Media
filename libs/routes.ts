098d8f7373eebacd73512110034bb45a92a8d5e5
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-21 23:52:27 +1000

    fixed routes. Now using ROUTE_NAME as source of truth for all dictionaries

--- libs/routes.ts
+++ libs/routes.ts
@@ -3,18 +3,23 @@ import VideocamIcon from '@mui/icons-material/Videocam'
 import MovieIcon from '@mui/icons-material/Movie'
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic'
 
-enum ROUTES {
-	HOME = '/',
-	VIDEOS = '/videos',
-	MOVIES = '/movies',
-	MUSIC = '/music',
+export enum ROUTE_NAME {
+	HOME = 'Home',
+	VIDEOS = 'Videos',
+	MOVIES = 'Movies',
+	MUSIC = 'Music',
 }
 
-export const ROUTE_ICONS: Record<ROUTES, typeof HomeIcon> = {
-	[ROUTES.HOME]: HomeIcon,
-	[ROUTES.VIDEOS]: VideocamIcon,
-	[ROUTES.MOVIES]: MovieIcon,
-	[ROUTES.MUSIC]: LibraryMusicIcon,
+export const ROUTE: Record<ROUTE_NAME, string> = {
+	[ROUTE_NAME.HOME]: '/',
+	[ROUTE_NAME.VIDEOS]: '/videos',
+	[ROUTE_NAME.MOVIES]: '/movies',
+	[ROUTE_NAME.MUSIC]: '/music',
 }
 
-export default ROUTES
+export const ROUTE_ICON: Record<ROUTE_NAME, typeof HomeIcon> = {
+	[ROUTE_NAME.HOME]: HomeIcon,
+	[ROUTE_NAME.VIDEOS]: VideocamIcon,
+	[ROUTE_NAME.MOVIES]: MovieIcon,
+	[ROUTE_NAME.MUSIC]: LibraryMusicIcon,
+}
