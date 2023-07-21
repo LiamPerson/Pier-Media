326c01d4ecef367b89ec849e31bf95516396be2e
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-22 00:42:27 +1000

    expanded header style & changed to appbar

--- components/DashboardPage/Header.tsx
+++ components/DashboardPage/Header.tsx
@@ -1,11 +1,37 @@
-import { Box } from '@mui/material'
+import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material'
 import { HEADER_HEIGHT } from './constants'
+import MenuIcon from '@mui/icons-material/Menu'
+import Image from 'next/image'
 
 interface HeaderProps {
 	width: number // in pixels
 }
 const Header = ({ width }: HeaderProps) => (
-	<Box sx={{ position: 'absolute', top: 0, left: 0, height: HEADER_HEIGHT, width, background: 'yellow' }}>9999999999999999999999999999</Box>
+	<AppBar sx={{ position: 'absolute', top: 0, left: 0, height: HEADER_HEIGHT, width }}>
+		<Toolbar>
+			<IconButton
+				size='large'
+				edge='start'
+				color='inherit'
+				aria-label='menu'
+				sx={{ mr: 2 }}
+			>
+				<MenuIcon />
+			</IconButton>
+			<Image
+				src='/logo512.png'
+				alt='logo'
+				width={50}
+				height={50}
+			/>
+			<Button
+				sx={{ marginLeft: 'auto' }}
+				color='inherit'
+			>
+				Login
+			</Button>
+		</Toolbar>
+	</AppBar>
 )
 
 export default Header
