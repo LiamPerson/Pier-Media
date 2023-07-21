0fe3abea3c64f2a1c9fb776c6ae299d835086127
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-22 00:41:43 +1000

    added palette options to theme & set background color on app bar

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,11 +1,22 @@
 import { createTheme } from '@mui/material'
+import { BACKGROUND } from './color'
 
 const theme = createTheme({
+	palette: {
+		mode: 'dark',
+		primary: {
+			main: '#26a6d8',
+		},
+		secondary: {
+			main: '#ff9734',
+		},
+	},
 	components: {
 		MuiAppBar: {
 			styleOverrides: {
 				root: {
 					boxShadow: 'none',
+					backgroundColor: BACKGROUND.DEFAULT,
 				},
 			},
 		},
