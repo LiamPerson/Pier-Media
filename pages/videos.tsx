2c57cf90ffb2630f11437f8f2e355014fd96d929
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-07-21 23:51:53 +1000

    added simple videos page

---
+++ pages/videos.tsx
@@ -0,0 +1,12 @@
+import DashboardPage from '@/components/DashboardPage'
+import { Box, Grid, Stack } from '@mui/material'
+
+const IndexPage = () => {
+	return (
+		<DashboardPage>
+			<p>Videos page</p>
+		</DashboardPage>
+	)
+}
+
+export default IndexPage
