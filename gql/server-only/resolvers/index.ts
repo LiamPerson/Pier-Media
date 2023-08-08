8c615b7b7bb6f8899f253012b9486cd5f4fc37ad
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-08 12:06:44 +1000

    Revert "attempted to modify prisma to log middleware"
    This reverts commit 2b3420046fa3d50715784e936e7c72935907a855.

--- gql/server-only/resolvers/index.ts
+++ gql/server-only/resolvers/index.ts
@@ -1,13 +1,11 @@
 import { Resolvers } from '@/gql/codegen/resolvers-types'
 import PierSettings from '@/libs/server-only/PierSettings'
-import { preferIsoStringMiddleware } from '@/prisma/middleware'
 import { PrismaClient } from '@prisma/client'
 
 const resolvers: Resolvers = {
 	Query: {
 		settings: async () => {
-			const rawPrisma = new PrismaClient()
-			const prisma = preferIsoStringMiddleware(rawPrisma)
+			const prisma = new PrismaClient()
 			const rawSettings = await PierSettings.getSettings(prisma)
 			return { ...rawSettings, downloads: { ...rawSettings.downloads, updatedAt: rawSettings.downloads.updatedAt.toISOString() } }
 		},
