62551b5283f113175f2b1ea7d920c919203c972c
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-08 13:47:43 +1000

    added optional throwError to filesystem is accessible method

--- libs/server-only/FileSystem.ts
+++ libs/server-only/FileSystem.ts
@@ -1,11 +1,22 @@
 import { accessSync, constants } from 'fs'
 
+interface isAccessibleOptions {
+	mode?: number
+	throwError?: boolean
+}
+
 class FileSystem {
-	static isAccessible(path: string, mode: number = constants.W_OK) {
+	/**
+	 * Checks if a path is accessible with the given mode.
+	 */
+	static isAccessible(path: string, { mode = constants.W_OK, throwError = false }: isAccessibleOptions = {}) {
 		try {
 			accessSync(path, mode)
 			return true
-		} catch {
+		} catch (e: any) {
+			if (throwError) {
+				throw e
+			}
 			return false
 		}
 	}
