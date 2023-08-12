3ff83fad43af0a677c648cefdeea554ae430e5f6
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-12 18:38:48 +1000

    simplified GET and POST handlers for graphql api

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -14,10 +14,4 @@ const server = new ApolloServer({
 
 const handler = startServerAndCreateNextHandler(server)
 
-export async function GET(request: NextRequest) {
-	return handler(request)
-}
-
-export async function POST(request: NextRequest) {
-	return handler(request)
-}
+export { handler as GET, handler as POST }
