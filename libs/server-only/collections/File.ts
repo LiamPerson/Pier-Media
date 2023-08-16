e1209866c21e855b1ae4d354b0bb53c4029eb7d7
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-16 16:16:17 +1000

    converted file & image upload into convoluted non-upsert series of functions to aid prisma error. This did not work.

--- libs/server-only/collections/File.ts
+++ libs/server-only/collections/File.ts
@@ -1,9 +1,13 @@
-import { PrismaClient } from '@prisma/client'
+import { File as FileModel, PrismaClient } from '@prisma/client'
 import { lookup } from 'mime-types'
 import { PathLike } from 'node:fs'
+import Debugger from '../Debugger'
 
 namespace File {
 	interface getProps {
+		onCreate?: (file: FileModel) => void
+		onUpdate?: (file: FileModel) => void
+		mediaType: string // The media/mime type of the file
 		location: string // Where the file has been saved
 		size: number // The size of the file in bytes
 		tags: string[] // Tags for the file
@@ -12,33 +16,40 @@ namespace File {
 	export const get = async (file: getProps, prisma: PrismaClient) => {
 		const mediaType = lookup(file.location)
 		if (!mediaType) throw new Error(`Error in File collection: Could not find media type for file '${file.location}'`)
-		const upsertedFileDetails = await prisma.file.upsert({
+		let fileDetails = await prisma.file.findUnique({
 			where: {
 				location: file.location,
 			},
-			update: {
-				size: file.size,
-				mediaType,
-			},
-			create: {
-				location: file.location,
-				size: file.size,
-				mediaType,
-				tagsJson: JSON.stringify(file.tags),
-			},
 		})
-		let existingTags = JSON.parse(upsertedFileDetails.tagsJson)
-		if (!existingTags || !Array.isArray(existingTags)) existingTags = []
-		// Fix up tags
-		const updatedFileDetails = await prisma.file.update({
-			where: {
-				id: upsertedFileDetails.id,
-			},
-			data: {
-				tagsJson: JSON.stringify(new Set([...existingTags, ...file.tags])),
-			},
-		})
-		return updatedFileDetails
+		if (fileDetails) {
+			Debugger.log('File.get: File already exists:', fileDetails)
+			let existingTags = JSON.parse(fileDetails.tagsJson)
+			if (!existingTags || !Array.isArray(existingTags)) existingTags = []
+			fileDetails = await prisma.file.update({
+				where: {
+					id: fileDetails.id,
+				},
+				data: {
+					size: file.size,
+					mediaType,
+					tagsJson: JSON.stringify(new Set([...existingTags, ...file.tags])),
+				},
+			})
+			file.onUpdate?.(fileDetails)
+		} else {
+			Debugger.log('File.get: File does not exist, creating:', file)
+			fileDetails = await prisma.file.create({
+				data: {
+					location: file.location,
+					size: file.size,
+					mediaType,
+					tagsJson: JSON.stringify(file.tags),
+				},
+			})
+			file.onCreate?.(fileDetails)
+		}
+
+		return fileDetails
 	}
 }
 
--- libs/server-only/collections/Image.ts
+++ libs/server-only/collections/Image.ts
@@ -4,6 +4,7 @@ import PierSettings from '../PierSettings'
 import imageSize from 'image-size'
 import File from './File'
 import Debugger from '../Debugger'
+import { File as FileModel } from '@prisma/client'
 
 namespace Image {
 	interface getProps {
@@ -11,40 +12,55 @@ namespace Image {
 		filename: string
 	}
 
-	export const get = async ({ source, filename }: getProps, prisma: PrismaClient) => {
-		Debugger.log('Trying to create image from source:', source)
-		const settings = await PierSettings.getSettings(prisma)
-		const output = `${settings.downloads.imagePath}/${filename}`
-		const { mediaType, size: byteSize } = await Network.getFile({ url: source, output })
-		const file = await File.get({ location: output, size: byteSize, tags: [] }, prisma)
+	const createImage = (file: FileModel, prisma: PrismaClient) => {
+		const imageDimensions = imageSize(file.location)
+		if (!imageDimensions.width || !imageDimensions.height) throw new Error(`Could not find valid image dimensions for file '${location}'.`)
 
-		const imageDimensions = imageSize(output)
-		if (!imageDimensions.width || !imageDimensions.height) throw new Error(`Could not find valid image dimensions for file '${output}'.`)
-		Debugger.log('Image.get: Image details:', mediaType, byteSize, imageSize, imageDimensions)
-		const imageDetails = prisma.image.upsert({
+		return prisma.image.create({
+			data: {
+				width: imageDimensions.width,
+				height: imageDimensions.height,
+				fileId: file.id,
+			},
+		})
+	}
+
+	const updateImage = (file: FileModel, prisma: PrismaClient) => {
+		const imageDimensions = imageSize(file.location)
+		if (!imageDimensions.width || !imageDimensions.height) throw new Error(`Could not find valid image dimensions for file '${location}'.`)
+		return prisma.image.update({
 			where: {
 				fileId: file.id,
 			},
-			update: {
+			data: {
 				width: imageDimensions.width,
 				height: imageDimensions.height,
-				file: {
-					connect: {
-						id: file.id,
-					},
-				},
-			},
-			create: {
-				width: imageDimensions.width,
-				height: imageDimensions.height,
-				file: {
-					connect: {
-						id: file.id,
-					},
-				},
 			},
 		})
-		return imageDetails
+	}
+
+	export const get = async ({ source, filename }: getProps, prisma: PrismaClient) => {
+		return new Promise(async (resolve, reject) => {
+			Debugger.log('Trying to create image from source:', source)
+			const settings = await PierSettings.getSettings(prisma)
+			const output = `${settings.downloads.imagePath}/${filename}`
+			const { mediaType, size: byteSize } = await Network.getFile({ url: source, output })
+			File.get(
+				{
+					location: output,
+					size: byteSize,
+					tags: [],
+					mediaType,
+					onCreate(file) {
+						createImage(file, prisma).then(resolve).catch(reject)
+					},
+					onUpdate(file) {
+						updateImage(file, prisma).then(resolve).catch(reject)
+					},
+				},
+				prisma
+			)
+		})
 	}
 }
 
