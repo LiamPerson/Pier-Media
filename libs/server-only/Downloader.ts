2555032a24747459a9fd9da62409278ad9b24107
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-20 02:02:49 +0900

    changed download logic on downloader to use full exec wrap, download path is based on provider + id, handled download timeout, allowed overriding on file collision

--- libs/server-only/Downloader.ts
+++ libs/server-only/Downloader.ts
@@ -5,12 +5,14 @@ import YTDlpWrap from 'yt-dlp-wrap-extended'
 import PierSettings from './PierSettings'
 import { PrismaClient } from '@prisma/client'
 import path from 'node:path'
-import System from './System'
 import { constants } from 'node:fs'
 import Provider from './collections/Provider'
 import Author from './collections/Author'
-import File from './collections/File'
 import Image from './collections/Image'
+import { InputMaybe } from '@/gql/codegen/resolvers-types'
+import System from './System'
+
+const MAX_DOWNLOAD_TIME = 1000 * 60 * 10 // 10 minutes
 
 export enum DownloadType {
 	AUDIO,
@@ -20,6 +22,7 @@ export enum DownloadType {
 interface DownloadProps {
 	url: string
 	type: DownloadType
+	overrideOnCollision?: InputMaybe<boolean> // Override the file if one already exists at the target location
 }
 
 interface YtdlpBinaries {
@@ -148,27 +151,92 @@ class Downloader {
 	static async getDownloadDetails(url: string, ytdlp: YTDlpWrap) {
 		return await ytdlp.getVideoInfo(url)
 	}
-	static async download({ url, type }: DownloadProps) {
+	static cleanUrl(url: string) {
+		// This url cleaning may only work for YouTube, need to test on more providers
+		return url.split('&')[0]
+	}
+	static checkEventForError({ event, data, onError }: { event: string; data: string; onError: (error: string) => void }) {
+		// if(event.toLowerCase() === 'download' && )
+		return false
+	}
+	static async download({ url, type, overrideOnCollision }: DownloadProps) {
 		const prisma = new PrismaClient()
-		const downloadFolder = await Downloader.getDownloadFolderPath(type, prisma)
 
-		const downloadPath = `${downloadFolder}/output123.mp4`
+		/** @todo - Liam: Remove any html query strings from url */
+		url = Downloader.cleanUrl(url)
+		console.log('Cleaned url: ', url)
 
 		Debugger.log(`Downloading ${url} as ${Downloader.downloadTypeToString(type)}`)
 		const binary = Downloader.getYtdlpBinaryPath()
 		Debugger.log(`Using binary at location ${binary}`)
 		const downloader = new YTDlpWrap(binary)
 		const downloadDetails = await Downloader.getDownloadDetails(url, downloader)
+
+		const sourceId = downloadDetails.id
+
 		Debugger.dumpJsonToFile(downloadDetails)
 		const authorSourceId = downloadDetails.channel_id || downloadDetails.uploader_id
-
 		const provider = await Provider.get(Provider.toProviderOption(downloadDetails.webpage_url_domain), prisma)
 		const author = await Author.get({ name: downloadDetails.uploader, sourceId: authorSourceId, provider }, prisma)
+
+		// We need to check that the file doesn't already exist as the target as yt-dlp will not override by default.
+		const downloadFolder = await Downloader.getDownloadFolderPath(type, prisma)
+		Debugger.log('Download folder: ', downloadFolder)
+		const downloadPath = `${downloadFolder}/${provider.id}.${sourceId}.mp4`
+		Debugger.log('Download path: ', downloadPath)
+		if (!System.isAccessible(downloadFolder, { mode: constants.W_OK })) {
+			throw new Error(
+				`You cannot download to the path '${downloadFolder}' as it is not writable. Please check your download settings and permissions.`
+			)
+		}
+		if (!overrideOnCollision && System.exists(downloadPath)) {
+			throw new Error(`There is a file collision at '${downloadPath}'. Please remove it or change your download settings.`)
+		}
+
 		// const videoFile = await File.get({ location: downloadPath, size: downloadDetails.filesize_approx, tags: downloadDetails.tags }, prisma)
 		const image = await Image.get({ source: downloadDetails.thumbnail, filename: 'test123.jpg' }, prisma)
 
 		console.log(`Attempting to download to path ${downloadPath}`)
-		await downloader.execPromise([url, '-o', downloadPath], { shell: true, detached: true })
+		/**
+		 * @todo - Liam:
+		 * You need to handle the download explicitly reading from console to catch warnings + errors + info.
+		 * Right now any download to the same location will not override and you will never know why.
+		 */
+		return new Promise((resolve, reject) => {
+			const controller = new AbortController()
+			const timeout = setTimeout(() => {
+				controller.abort()
+				console.log('Event killed?: ', downloadEventEmitter.ytDlpProcess?.killed)
+				reject(`Download timed out. Waited ${MAX_DOWNLOAD_TIME} milliseconds.`)
+			}, MAX_DOWNLOAD_TIME)
+			const downloadEventEmitter = downloader
+				.exec([url, '-f', 'best', '-o', downloadPath], { shell: true }, controller.signal)
+				.on('progress', (progress) => {
+					console.log('Progress', progress)
+				})
+				.on('ytDlpEvent', (eventType, eventData) => {
+					/**
+					 * @todo - Anyone: We need to pay close attention here as this is where we will find
+					 * gotchas. The event data is just string literals that are verbatim from the console.
+					 *
+					 * @todo - Anyone: Need to test the errors have the same (or similar)
+					 * errors across the different binaries.
+					 *
+					 * Maybe we should log an issue with the yt-dlp project to get them to standardise errors or
+					 * generate error codes?
+					 */
+					console.log('Event found!', { eventType, eventData })
+				})
+				.on('error', (error) => {
+					clearTimeout(timeout)
+					reject(error)
+				})
+				.on('close', () => {
+					clearTimeout(timeout)
+					console.log('all done')
+					resolve('Done!')
+				})
+		})
 	}
 }
 
