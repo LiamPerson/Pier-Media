a779e146e38bf8e154269b32c767bb4d2772d346
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-27 17:01:10 +1000

    added tryAddDefaults to genre collection

--- libs/server-only/collections/Genre.ts
+++ libs/server-only/collections/Genre.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import DEFAULT_GENRES from '@/data/audio_genres.json'
+import { Maybe } from '@/gql/codegen/resolvers-types'
+import { InputMaybe } from '@/gql/codegen/graphql'
 
 namespace Genre {
 	interface getProps {
@@ -21,10 +24,21 @@ namespace Genre {
 		return genreDetails
 	}
 	interface tryAddDefaultsProps {
-		overrideExisting: boolean
+		overrideExisting?: InputMaybe<boolean>
 	}
 	export const tryAddDefaults = async ({ overrideExisting }: tryAddDefaultsProps, prisma: PrismaClient) => {
 		/** @todo - Liam: Import audio_genres.json and recursively try add them to the tracks. Override existing should prevent overriding collisions on the name */
+		if (overrideExisting) {
+			await prisma.$transaction(
+				DEFAULT_GENRES.map((genre) => prisma.genre.upsert({ update: genre, where: { name: genre.name }, create: genre })),
+				{ isolationLevel: Prisma.TransactionIsolationLevel.Serializable }
+			)
+		} else {
+			await prisma.$transaction(
+				DEFAULT_GENRES.map((genre) => prisma.genre.create({ data: genre })),
+				{ isolationLevel: Prisma.TransactionIsolationLevel.Serializable }
+			)
+		}
 	}
 }
 
