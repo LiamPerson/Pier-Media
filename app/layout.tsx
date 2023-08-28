0fdeb3b3c69fab281414830cfc2b4c4b74b210c9
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-08-28 23:54:03 +1000

    parsed metadata as metadata nextjs type

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import AppProviders from '@/components/core/AppProviders'
+import { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: 'Pier Media',
 	description: 'An open source media server made to work on a RaspberryPi.',
 }
