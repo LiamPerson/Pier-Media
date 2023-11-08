0306ca9010d150635968df08fe34caa3b9a56b02
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-11-09 00:17:58 +1000

    added video collection

---
+++ libs/server-only/collections/Video.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from '@prisma/client'
+import Author from './Author'
+import Provider from './Provider'
+
+namespace Video {
+	interface getProps {
+		provider: Provider.Type
+		name: string
+		sourceId: string
+		author: Author.Type
+	}
+
+	export const get = async (author: getProps, prisma: PrismaClient) => {
+		let authorDetails = await prisma.author.findFirst({
+			where: {
+				sourceId: author.sourceId,
+			},
+		})
+		if (!authorDetails) {
+			authorDetails = await prisma.author.create({
+				data: {
+					name: author.name,
+					sourceId: author.sourceId,
+					provider: {
+						connect: {
+							id: author.provider.id,
+						},
+					},
+				},
+			})
+		}
+		return authorDetails
+	}
+
+	export type Type = Awaited<ReturnType<typeof get>>
+}
+
+export default Video
