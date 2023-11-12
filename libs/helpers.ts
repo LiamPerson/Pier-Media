0da13091b6885fd9bd50e47f7da6c34d95b16cfa
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2023-11-13 01:11:57 +1000

    added seconds to timestamp function in helpers

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -6,3 +6,24 @@ export const toCapitalCase = (string: string) => {
 		return a.toUpperCase()
 	})
 }
+
+/**
+ * Converts seconds to a timestamp:
+ * @example secondsToTimestamp(65) // result: 1:05
+ * @example secondsToTimestamp(3600) // result: 1:00:00
+ */
+export const secondsToTimestamp = (inputSeconds: number) => {
+	const oneHourInSeconds = 3600
+	const hours = Math.floor(inputSeconds / oneHourInSeconds)
+		.toString()
+		.padStart(2, '0')
+	const minutes = Math.floor((inputSeconds % oneHourInSeconds) / 60)
+		.toString()
+		.padStart(1, '0')
+	const seconds = Math.floor(inputSeconds % 60)
+		.toString()
+		.padStart(2, '0')
+
+	if (inputSeconds >= oneHourInSeconds) return hours + ':' + minutes + ':' + seconds
+	return minutes + ':' + seconds
+}
