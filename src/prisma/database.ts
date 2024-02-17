d18b9b462cf68f05c27ee37b777e7be9697a5fba
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2024-02-17 22:40:14 +1000

    fixed export order in prisma database

--- src/prisma/database.ts
+++ src/prisma/database.ts
@@ -17,6 +17,6 @@ const globalForPrisma = globalThis as unknown as {
 
 const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
-export default prisma
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
