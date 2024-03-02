f9c354f64c1a24c475f302d26e0e5a7482b25da7
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2024-03-02 18:57:03 +1000

    Started to add titles and sections to IndexPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
+import { Box, Stack, Typography } from '@mui/material'
+import { ReactNode } from 'react'
+
 import DashboardPage from '@/components/core/DashboardPage'
-import { Box, Grid, Stack } from '@mui/material'
+import NextLink from '@/components/shared/NextLink'
+import { ROUTE } from '@/libs/routes'
+
+const HomeSection = ({ title, link, children }: { title: string; link: string; children: ReactNode }) => {
+	return (
+		<Box>
+			<NextLink href={link}>
+				<Typography
+					variant='h1'
+					sx={{ display: 'inline-block' }}
+				>
+					{title}
+				</Typography>
+			</NextLink>
+			<Box>{children}</Box>
+		</Box>
+	)
+}
 
 const IndexPage = () => {
 	return (
@@ -9,20 +29,25 @@ const IndexPage = () => {
 					height: '1500px',
 				}}
 			>
-				<Stack
-					gap={'15px'}
-					direction={'row'}
-					sx={{ flexWrap: 'wrap' }}
+				<HomeSection
+					title='Music'
+					link={ROUTE.Music}
 				>
-					{Array.from({ length: 20 }).map((_, index) => (
-						<Box
-							key={index}
-							sx={{ display: 'flex', background: 'cyan', width: '100px', height: '100px' }}
-						>
-							A box {index}
-						</Box>
-					))}
-				</Stack>
+					<Stack
+						gap={'15px'}
+						direction={'row'}
+						sx={{ flexWrap: 'wrap' }}
+					>
+						{Array.from({ length: 20 }).map((_, index) => (
+							<Box
+								key={index}
+								sx={{ display: 'flex', background: 'cyan', width: '100px', height: '100px' }}
+							>
+								A box {index}
+							</Box>
+						))}
+					</Stack>
+				</HomeSection>
 			</Box>
 		</DashboardPage>
 	)
