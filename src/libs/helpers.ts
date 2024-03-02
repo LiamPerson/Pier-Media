9e985bf8d5600b90e13e0579e9b8684a117d4da7
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2024-03-02 19:33:18 +1000

    added shuffle helper function

--- src/libs/helpers.ts
+++ src/libs/helpers.ts
@@ -45,3 +45,15 @@ export const nullsToUndefined = <T extends Record<string, any>>(object: T) => {
 		return value
 	})
 }
+
+/**
+ * Shuffles an array.
+ */
+export const shuffle = <T>(array: T[]) => {
+	const newArray = [...array]
+	for (let i = newArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+	}
+	return newArray
+}
