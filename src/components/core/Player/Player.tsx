ce1a3d9e7eaefdfbcd3f69be6142d74539599e4d
Liam <34695601+YeloPartyHat@users.noreply.github.com>
2024-03-02 20:49:19 +1000

    Refactor Player component and remove unused code

--- src/components/core/Player/Player.tsx
+++ src/components/core/Player/Player.tsx
@@ -3,18 +3,16 @@
 import { Paper } from '@mui/material'
 
 import { PlayerContent } from './PlayerContent'
-import { usePlayer } from './usePlayer'
 
 type Props = {
 	children: React.ReactNode
 }
 export const Player = ({ children }: Props) => {
-	const { current: media } = usePlayer()
 	return (
 		<>
 			{children}
 			<Paper sx={{ width: '100%', position: 'sticky', bottom: '0', right: '0' }}>
-				<PlayerContent media={media} />
+				<PlayerContent />
 			</Paper>
 		</>
 	)
--- src/components/core/Player/PlayerContent.tsx
+++ src/components/core/Player/PlayerContent.tsx
@@ -1,12 +1,10 @@
-import { Media } from '@/constants/media'
 import { AudioPlayerSmall } from './AudioPlayerSmall'
+import { usePlayer } from './usePlayer'
+
 import { isAudioMedia } from '@/libs/media-helpers'
 
-type OverlayProps = {
-	media: Media | null
-}
-
-export const PlayerContent = ({ media }: OverlayProps) => {
+export const PlayerContent = () => {
+	const { current: media } = usePlayer()
 	if (!media) return <div>No audio selected.</div>
 	if (!isAudioMedia(media)) return <div>Currently unsupported media type ({media.type}).</div>
 	return <AudioPlayerSmall media={media} />
