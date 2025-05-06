c2249a55d98acd1477156c91b558b078493498cf
LiamPerson <34695601+LiamPerson@users.noreply.github.com>
2025-05-06 22:38:05 +0900

    Remade persist-volume as VSCode lost it ...

--- persist-volume.js
+++ persist-volume.js
@@ -1,3 +1,21 @@
 /**
  * Persists the volume between pages.
  */
+window.addEventListener(
+	'DOMContentLoaded',
+	function () {
+		const video = document.getElementById('video')
+		const SESSION_KEY_VOLUME = 'videoVolume'
+		const defaultVolume = 1
+
+		video.volume = this.sessionStorage.getItem(SESSION_KEY_VOLUME) || defaultVolume
+
+		/**
+		 * Listen for when volume is changed. If it is, store that as the new value.
+		 */
+		video.addEventListener('volumechange', function () {
+			sessionStorage.setItem(SESSION_KEY_VOLUME, video.volume)
+		})
+	},
+	true
+)
