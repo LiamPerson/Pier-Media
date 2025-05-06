f0dd0df66fd39a0955057a41502f29a9b629cfda
LiamPerson <34695601+LiamPerson@users.noreply.github.com>
2025-05-06 22:33:40 +0900

    Made window usage consistent and put all variable in the function and added doc

--- persist-search.js
+++ persist-search.js
@@ -1,14 +1,16 @@
-const searchInput = document.getElementById('search')
-
+/**
+ * Persists the search text between pages and navigation and reload
+ */
 window.addEventListener(
 	'DOMContentLoaded',
 	function () {
+		const searchInput = document.getElementById('search')
 		const SESSION_KEY_SEARCH = 'search'
 		// Try load value from params ...
-		const urlParams = new URLSearchParams(window.location.search)
+		const urlParams = new URLSearchParams(this.location.search)
 		let query = urlParams.get('q')
 
-		if (query) sessionStorage.setItem(SESSION_KEY_SEARCH, query)
+		if (query) this.sessionStorage.setItem(SESSION_KEY_SEARCH, query)
 
 		// If no value, try load from session
 		query = this.sessionStorage.getItem(SESSION_KEY_SEARCH)
