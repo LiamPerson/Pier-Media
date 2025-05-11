07bb0ae6f175d53fa6f244754cc02d57a6454b63
LiamPerson <34695601+LiamPerson@users.noreply.github.com>
2025-05-11 17:02:54 +0900

    Changed constants to environment

--- constants.js
+++
@@ -1,2 +0,0 @@
-const FILE_SERVER = 'http://192.168.50.42:3000/file?url='
-const SEARCH_API_URL = 'http://localhost:3000/search'
---
+++ environment.js
@@ -0,0 +1,2 @@
+const FILE_SERVER = 'http://localhost:3000/file?url='
+const SEARCH_API_URL = 'http://localhost:3000/search'
